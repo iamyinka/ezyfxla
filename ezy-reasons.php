08a920258c9eac551137bfe8fd62cddfb9f0e2f2
Yinka Ash <developer@designsbynetguru.com>
2017-10-11 17:56:40 +0800

    Finish EZY Reasons Page

--- ezy-reasons.php
+++ ezy-reasons.php
@@ -130,4 +130,226 @@
   </div>
 </section>
 
+<section class="reason-divider">
+  <div class="container">
+    <h4 class="page-header">START EARNING MORE WITH EZYFX NOW</h4>
+
+    <!-- row requires "row-divided" class -->
+    <div class="row row-divided">
+
+        <div class="col-xs-6">
+
+        </div>
+      	<div class="vertical-divider"></div>
+      	<div class="col-xs-6 column-two">
+              <h2 style="color: #00c98d;">OPEN TRADE</h2>
+              <p><strong>Spread</strong></p>
+              <p>BECAUSE WE HAVE LOWEST SPREADS IN THE INDUSTRY</p>
+
+              <div class="comparison">
+                <div class="ezyfx">
+                  <h6>EZYFX</h6>
+                  <p>0.7PIP - $5</p>
+                  <p><span>-$5</span></p>
+                </div>
+
+                <div class="other">
+                  <h6>Other Broker</h6>
+                  <p>2.5PIP - $5</p>
+                  <p><span>-$65</span></p>
+                </div>
+              </div>
+      	</div>
+
+
+
+
+        <div class="col-xs-6 column-one">
+          <h2 style="color: #00c98d;">TRADE OPEN FOR 2 NIGHTS</h2>
+          <p><strong>Swap</strong></p>
+          <p>PAY NO SWAPS ON ORDERS HELD OVERNIGHT</p>
+
+          <div class="comparison">
+            <div class="ezyfx">
+              <h6>EZYFX</h6>
+              <p>0.7PIPS - $5</p>
+              <p>NO DELAY - $0</p>
+              <p>NO SWAP - $0</p>
+              <p><span>-$7</span></p>
+            </div>
+
+            <div class="other">
+              <h6>Other Broker</h6>
+              <p>2.5PIPS - $5</p>
+              <p><span class="breaker">1 Sec Delay - $130</span> <small style="color: #444;">Up to 5 pip Charge</small></p>
+              <p>2PIPS - $50</p>
+              <p><span class="last-figure">-$245</span></p>
+            </div>
+          </div>
+        </div>
+      	<div class="vertical-divider"></div>
+      	<div class="col-xs-6">
+
+      	</div>
+
+    </div>
+
+
+    <div class="row row-divided">
+
+        <div class="col-xs-6">
+
+        </div>
+      	<div class="vertical-divider"></div>
+      	<div class="col-xs-6 column-two">
+          <h2 style="color: #00c98d;">CLOSE TRADE</h2>
+          <p><strong>Spread</strong></p>
+          <p>YES! WE STILL HAVE THE BEST SPREADS</p>
+
+          <div class="comparison">
+            <div class="ezyfx">
+              <h6>EZYFX</h6>
+              <p>0.7PIPS - $5</p>
+              <p>NO DELAY - $0</p>
+              <p>NO SWAP - $0</p>
+              <p>0.7PIPS - $15</p>
+              <p class="no-show">..</p>
+              <p><span>-$20</span></p>
+            </div>
+
+            <div class="other">
+              <h6>Other Broker</h6>
+              <p>2.5PIPS - $5</p>
+              <p><span class="breaker">1 Sec Delay - $130</span> <small style="color: #444;">Up to 5 pip Charge</small></p>
+              <p>2PIPS - $50</p>
+              <p>2.5PIPS - $75</p>
+              <p><span class="last-figure">-$320</span></p>
+            </div>
+          </div>
+      	</div>
+
+
+
+        <div class="col-xs-6 column-one">
+          <h2 style="color: #00c98d;">CLOSE TRADE</h2>
+          <p><strong>Slippage</strong></p>
+          <p>97.5% OF ORDERS EXECUTED WITHOUT SLIPPAGE</p>
+
+          <div class="comparison">
+            <div class="ezyfx">
+              <h6>EZYFX</h6>
+              <p>0.7PIPS - $5</p>
+              <p>NO DELAY - $0</p>
+              <p>NO SWAP - $0</p>
+              <p>0.7PIPS - $15</p>
+              <p>NO SLIPPAGE - $0</p>
+              <p class="no-show">..</p>
+              <p><span>-$20</span></p>
+            </div>
+
+            <div class="other">
+              <h6>Other Broker</h6>
+              <p>2.5PIPS - $5</p>
+              <p><span class="breaker">1 Sec Delay - $130</span> <small style="color: #444;">Up to 5 pip Charge</small></p>
+              <p>2PIPS - $50</p>
+              <p>2.5PIPS - $75</p>
+              <p>5PIPS - $150</p>
+              <p><span class="last-figure">-$470</span></p>
+            </div>
+          </div>
+        </div>
+      	<div class="vertical-divider"></div>
+      	<div class="col-xs-6">
+
+      	</div>
+
+    </div>
+
+
+
+
+
+
+
+
+    <div class="row row-divided">
+
+        <div class="col-xs-6">
+
+        </div>
+      	<div class="vertical-divider"></div>
+      	<div class="col-xs-6 column-two">
+          <h2 style="color: #00c98d;">ORDER PROFIT</h2>
+          <p><strong>ORDER PROFIT</strong></p>
+          <p>ORDER PROFIT 30 PIPS + $ 900</p>
+
+          <div class="comparison">
+            <div class="ezyfx">
+              <h6>EZYFX</h6>
+              <p>0.7PIPS - $5</p>
+              <p>NO DELAY - $0</p>
+              <p>NO SWAP - $0</p>
+              <p>0.7PIPS - $15</p>
+              <p>NO SLIPPAGE - $0</p>
+              <p>30 PIPS + $900</p>
+              <p class="no-show">..</p>
+              <p><span>+$880</span></p>
+            </div>
+
+            <div class="other">
+              <h6>Other Broker</h6>
+              <p>2.5PIPS - $5</p>
+              <p><span class="breaker">1 Sec Delay - $130</span> <small style="color: #444;">Up to 5 pip Charge</small></p>
+              <p>2PIPS - $50</p>
+              <p>2.5PIPS - $75</p>
+              <p>5PIPS - $150</p>
+              <p>30 PIPS + $900</p>
+              <p><span class="last-figure">+$430</span></p>
+            </div>
+          </div>
+      	</div>
+
+
+
+        <div class="col-xs-6 column-one">
+          <h2 style="color: #00c98d;">WITHDRAWAL</h2>
+          <p><strong>Withdrawal commission</strong></p>
+          <p>WE CHARGE NO COMMISSIONS ON DEPOSIT AND WITHDRAWAL</p>
+
+          <div class="comparison">
+            <div class="ezyfx">
+              <h6>EZYFX</h6>
+              <p>0.7PIPS - $5</p>
+              <p>NO DELAY - $0</p>
+              <p>NO SWAP - $0</p>
+              <p>0.7PIPS - $15</p>
+              <p>NO SLIPPAGE - $0</p>
+              <p>30 PIPS + $900</p>
+              <p>NO COMMISSION $0</p>
+              <p class="no-show">..</p>
+              <p><span>+$880</span></p>
+            </div>
+
+            <div class="other">
+              <h6>Other Broker</h6>
+              <p>2.5PIPS - $5</p>
+              <p><span class="breaker">1 Sec Delay - $130</span> <small style="color: #444;">Up to 5 pip Charge</small></p>
+              <p>2PIPS - $50</p>
+              <p>2.5PIPS - $75</p>
+              <p>5PIPS - $150</p>
+              <p>30 PIPS + $900</p>
+              <p>2% - $9.6</p>
+              <p><span class="last-figure">+$420.40</span></p>
+            </div>
+          </div>
+        </div>
+      	<div class="vertical-divider"></div>
+      	<div class="col-xs-6">
+
+      	</div>
+
+    </div>
+</div>
+</section>
+
 <?php include('includes/footer.php'); ?>
