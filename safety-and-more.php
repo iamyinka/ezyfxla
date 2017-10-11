e2db84f6416649322b3e9eb73cab59c39b110bd3
Yinka Ash <developer@designsbynetguru.com>
2017-10-11 19:42:38 +0800

    Finish Safety and Security of Client's Funds Page

---
+++ safety-and-more.php
@@ -0,0 +1,85 @@
+<?php include('includes/nav.php'); ?>
+
+<section class="why-us">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <h1><i class="fa fa-lock"></i>Safety &amp; Security of client's Funds</h1>
+      </div>
+      <div class="col-sm-6">
+        <p><a href="#" class="btn-link">Download Metatrader trading platform</a></p>
+        <div class="row">
+          <div class="col-xs-6 col-sm-6 col-md-6 downloads">
+            <a href="https://www.instaforex.com/register_demo_account" class="btn btn-custom-default btn-lg-xs btn-block">Open Demo account</a>
+          </div>
+          <div class="col-xs-6 col-sm-6 col-md-6 downloads">
+            <a href="https://www.instaforex.com/fast_open_live_account" class="btn btn-custom-danger btn-lg-xs btn-block">Open trading account</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<section class="safety-starter">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-6">
+        <h4>Safety of Clients Funds</h4>
+        <p>At Ezyfx, we know you work hard for your money and quite frankly deserve that peace of mind. We give our customers complete satisfaction of security because we practice what we preach unlike others. All our funds are stored in separate client accounts with no overlapping.</p>
+
+        <p>We enforce stringent standards of Data Security subject to a high-level scrutiny and all your personal details are encrypted using SSL (Secure Sots Layer). We personally do not store any of your financial detail as they are all processed using our partner network which is a fully licensed payment gateway protecting client with PCI regulated servers.</p>
+
+        <p>Your fund and its security is our number one priority, with Ezyfx you can be completely sure that we are protecting your funds. Here’s how:</p>
+      </div>
+
+      <div class="col-xs-12 col-sm-6">
+        <div class="safety-blocks">
+          <img src="images/safety/1.png" alt="">
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-7">
+        <h4>SEGREGATED ACCOUNTS</h4>
+
+        <p>In accordance with international regulation standards, EZYFX uses separate accounts to keep protected customers’ funds segregated from the company’s balance sheets. This keeps your funds secure and untouched.</p>
+
+        <h4>SSL-PROTECTED PERSONAL AREA</h4>
+
+        <p>We use highly secure technology to protect your personal data and financial transactions. SSL-secured Personal Area is protected with 128-bit encryption, which makes your browsing safe and your data inaccessible to any third parties.</p>
+
+        <h4>ACCOUNT VERIFICATION</h4>
+
+        <p>EZYFX recommends you to verify your account by submitting your personal ID scan and an address proof. This simple measure will make sure your transactions are authorized and secured.</p>
+
+        <h4>SECURE WITHDRAWAL RULES</h4>
+
+        <p>Since a withdrawal from a live account requires email confirmation, no one can ever access your account except you. It also requires you to use the same payment details for deposits and withdrawals. Thus, under no circumstances can EZYFX transfer your withdrawal to an unauthorized third party.</p>
+
+        <h4>3D SECURE VISA/MASTERCARD AUTHORIZATION</h4>
+
+        <p>We apply 3D secure technology when processing credit and debit cards. This technology makes all Visa/Master card transactions transparent and safe.</p>
+
+        <h4>ADVANCED PROTECTION</h4>
+
+        <p>The EZYFX technical environment is monitored 24/7 by a dedicated team of highly professional security engineers and technical specialists. They have developed and maintain top level protection, so any data loss, damage or other technical issues are highly unlikely.</p>
+      </div>
+
+      <div class="col-xs-12 col-sm-5">
+        <div class="safety-blocks">
+          <img src="images/safety/2.png" alt="">
+        </div>
+
+        <div class="safety-blocks">
+          <img src="images/safety/3.png" alt="">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<?php include('includes/footer.php'); ?>
