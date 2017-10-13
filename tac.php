72c9dc42d6591adc1be1d5bf65800ea41a32a6ba
Yinka Ash <developer@designsbynetguru.com>
2017-10-13 14:25:33 +0800

    Finish TAC Page

---
+++ tac.php
@@ -0,0 +1,178 @@
+<?php include('includes/nav.php'); ?>
+
+<section class="why-us">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <h1><i class="fa fa-credit-card-alt"></i>Trading Account Conditions</h1>
+      </div>
+      <div class="col-sm-6">
+        <p><a href="#" class="btn-link">Download Metatrader trading platform</a></p>
+        <div class="row">
+          <div class="col-xs-6 col-sm-6 col-md-6 downloads">
+            <a href="https://www.instaforex.com/register_demo_account" class="btn btn-custom-default btn-lg-xs btn-block">Open Demo account</a>
+          </div>
+          <div class="col-xs-6 col-sm-6 col-md-6 downloads">
+            <a href="https://www.instaforex.com/fast_open_live_account" class="btn btn-custom-danger btn-lg-xs btn-block">Open trading account</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="reg-start payment-options">
+  <div class="container">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+
+            <!-- Tab panes -->
+            <div class="tab-content faq-cat-content">
+                <div class="tab-pane active in fade" id="faq-cat-1">
+                    <div class="panel-group" id="accordion-cat-1">
+
+                        <div class="bs-component">
+
+                          <div class="tab-content">
+                              <div id="tab1" class="tab-pane fade active in">
+                                  <table class="table table-responsive table-striped table-bordered table-hover footable toggle-medium">
+                                      <thead>
+                                           <th class="tb-color1">CRITERIA</th>
+                                           <th data-hide="all" class="tb-color2">EZY</th>
+                                           <th data-hide="all" class="tb-color2">EZY CLASSIC</th>
+                                           <th data-hide="all" class="tb-color2">EZY ISLAMIC</th>
+                                           <th data-hide="all" class="tb-color3">ECN STANDARD</th>
+                                           <th data-hide="all" class="tb-color3">ECN PREMIUM</th>
+                                           <th data-hide="all" class="tb-color3">ECN SEGREGATED</th>
+                                       </thead>
+                                       <tbody>
+                                          <tr>
+                                            <td class="tb-color1">
+                                              Trading Instruments
+                                            </td>
+                                            <td class="tb-color2">Forex, Indices, Commodities and Share CFDs</td>
+                                            <td class="tb-color2">Forex, Indices, Commodities and Share CFDs</td>
+                                            <td class="tb-color2">Forex, Indices, Commodities and Share CFDs</td>
+                                            <td class="tb-color3">Forex, Indices, Commodities and Share CFDs</td>
+                                            <td class="tb-color3">Forex, Indices, Commodities and Share CFDs</td>
+                                            <td class="tb-color3">Forex, Indices, Commodities and Share CFDs</td>
+                                          </tr>
+                                          <tr>
+                                            <td class="tb-color1">
+                                              Minimum Deposit
+                                            </td>
+                                            <td class="tb-color2">0</td>
+                                            <td class="tb-color2">$10 USD</td>
+                                            <td class="tb-color2">$100 USD</td>
+                                            <td class="tb-color3">$100 USD</td>
+                                            <td class="tb-color3">$1,000 USD</td>
+                                            <td class="tb-color3">$10,000 USD</td>
+                                          </tr>
+
+                                          <tr>
+                                            <td class="tb-color1">
+                                              Maximum Equity <sup>[1]</sup>
+                                            </td>
+                                            <td class="tb-color2">$500 USD</td>
+                                            <td class="tb-color2">$500 USD</td>
+                                            <td class="tb-color2">Unlimited</td>
+                                            <td class="tb-color3">$5,000 USD</td>
+                                            <td class="tb-color3">Unlimited</td>
+                                            <td class="tb-color3">Unlimited</td>
+                                          </tr>
+
+
+                                          <tr>
+                                            <td class="tb-color1">
+                                              Minimum Order Size
+                                            </td>
+                                            <td class="tb-color2">0.01 lots</td>
+                                            <td class="tb-color2">0.01 lots</td>
+                                            <td class="tb-color2">0.01 lots</td>
+                                            <td class="tb-color3">0.01 lots</td>
+                                            <td class="tb-color3">0.01 lots</td>
+                                            <td class="tb-color3">0.01 lots</td>
+                                          </tr>
+
+
+
+                                          <tr>
+                                            <td class="tb-color1">
+                                              Maximum Order Size (Sum)
+                                            </td>
+                                            <td class="tb-color2">5 lots</td>
+                                            <td class="tb-color2">10 lots</td>
+                                            <td class="tb-color2">&#8734;</td>
+                                            <td class="tb-color3">100 lots</td>
+                                            <td class="tb-color3">&#8734;</td>
+                                            <td class="tb-color3">&#8734;</td>
+                                          </tr>
+
+
+
+
+                                          <tr>
+                                            <td class="tb-color1">
+                                              Leverage
+                                            </td>
+                                            <td class="tb-color2">1:200</td>
+                                            <td class="tb-color2">1:1000</td>
+                                            <td class="tb-color2">1:200</td>
+                                            <td class="tb-color3">1:400</td>
+                                            <td class="tb-color3">1:200</td>
+                                            <td class="tb-color3">1:200</td>
+                                          </tr>
+
+
+                                          <tr>
+                                            <td class="tb-color1">
+                                              Instrument
+                                            </td>
+                                            <td class="tb-color2">5 Digits</td>
+                                            <td class="tb-color2">5 Digits</td>
+                                            <td class="tb-color2">5 Digits</td>
+                                            <td class="tb-color3">5 Digits</td>
+                                            <td class="tb-color3">5 Digits</td>
+                                            <td class="tb-color3">5 Digits</td>
+                                          </tr>
+                                       </tbody>
+                                   </table>
+                               </div>
+                             </div>
+                          </div>
+
+                    </div>
+                </div>
+            </div>
+          </div>
+    </div>
+  </div>
+</section>
+
+<section class="take-note tac">
+  <div class="container">
+    <div class="col-xs-12 col-sm-12">
+      <p style="color: #b33;"><strong>* <em>Terms &amp; Conditions applies</em></strong></p>
+
+      <h6>IMPORTANT NOTE:</h6>
+
+      <ol>
+        <li><span>EZYFX reserves the rights to alter the client’s account category accordingly if the account’s equity is exceeded the maximum level</span></li>
+        <li><span>EZYFX reserves the rights to change the account to swap account if the orders were open for more than 30 days</span></li>
+        <li><span>For Islamic Accounts, Swap is exempted on Forex pairs only; Indices & Commodities will be charged for lowest swap in the market</span></li>
+        <li><span>EZYFX reserves the rights to cancel any profits that made within 2 minutes as well the rebate imposed.</span></li>
+        <li><span>EZYFX reserves the rights to change your account category if the Balance/Volume ratio is always lower than 500</span></li>
+        <li><span>EZYFX reserves the rights to change your leverage on market volatility</span></li>
+        <li><span>For EZY BASIC account that wishes to claim the no deposit bonus, you are allowed to claim the withdrawal of the FREE bonus and all the profits subjected to the terms and conditions stated from no 8 to 12.</span></li>
+        <li><span>The Profit can be withdrawn only after the trading turnover in the account achieves a minimum total of 20 lots traded continuously for a minimum of 90 days.</span></li>
+        <li><span>Minimum Profit, which can be withdrawn after turnover conditions met, is equivalent to USD100.</span></li>
+        <li><span>All funds will be debited from the account, including the Bonus when profit is withdrawn for the first time.</span></li>
+        <li><span>It is strictly prohibited to get the no deposit bonus again by using a new registration!</span></li>
+        <li><span>It is also prohibited to get the bonus if your relative or a close person has already received the bonus.</span></li>
+      </ol>
+    </div>
+  </div>
+</section>
+
+
+<?php include('includes/footer.php'); ?>
