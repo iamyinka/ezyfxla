f4076f42f801e90dfacb759722afb14b06c0edc8
Yinka Ash <developer@designsbynetguru.com>
2017-10-13 15:52:19 +0800

    Finish Islamic Accounts

---
+++ islamic-accounts.php
@@ -0,0 +1,81 @@
+<?php include('includes/nav.php'); ?>
+
+<section class="why-us">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <h1><i class="fa fa-google-wallet"></i>Islamic Accounts</h1>
+      </div>
+      <div class="col-sm-6">
+        <p><a href="#" class="btn-link">Download Metatrader trading platform</a></p>
+        <div class="row">
+          <div class="col-xs-6 col-sm-6 col-md-6 downloads">
+            <a href="https://www.instaforex.com/register_demo_account" class="btn btn-custom-default btn-lg-xs btn-block">Open Demo account</a>
+          </div>
+          <div class="col-xs-6 col-sm-6 col-md-6 downloads">
+            <a href="https://www.instaforex.com/fast_open_live_account" class="btn btn-custom-danger btn-lg-xs btn-block">Open trading account</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="reg-start">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-12">
+        <p class="text-center">EZYFX Islamic account is a unique offer designed exclusively for Muslim traders</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="reg-start islamic-area">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-12 col-md-8">
+        <div class="islamic-content">
+          <div class="row">
+            <div class="col-xs-12 col-sm-12">
+              <div class="islamic-sections">
+                <i class="fa fa-thumbs-up"></i>
+                <h6>100% Shariah Compliant</h6>
+                <p>Pay no interest on your positions held overnight</p>
+              </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12">
+              <div class="islamic-sections">
+                <i class="fa fa-hand-o-up"></i>
+                <h6>AVAILABLE AT ONE CLICK</h6>
+                <p>Simply check “Swap Free” box when you open your account</p>
+              </div>
+            </div>
+
+            <div class="col-xs-12 col-sm-12">
+              <div class="islamic-sections">
+                <i class="fa fa-users"></i>
+                <h6>AVAILABLE FOR ALL ACCOUNT TYPES</h6>
+                <p>Trade without swaps on any account that suits you best</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-xs-12 col-sm-12 col-md-4">
+        <div class="islamic-content2">
+          <p class="lead"><strong>We Are Happy To Be Distinguished By A Trusted Forex Web Portal</strong></p>
+          <p>Islamic account has no swap applied during a rollover but a fixed fee instead. This fee is not an interest and primarily depends on the direction of your position.</p>
+          <p><em>We care about our customers and we do our best to make their trading experience easy and convenient.</em></p>
+          <p>Open an award-winning Islamic account with EZYFX today!</p>
+          <a href="#" class="btn btn-custom-live btn-block">Open An Account</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+<?php include('includes/footer.php'); ?>
