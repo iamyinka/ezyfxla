ec3f895286e45da2c4a63330c311ddee7c9798a9
Yinka Ash <developer@designsbynetguru.com>
2017-11-14 19:26:47 +0800

    Add TOS

--- partner-with-us.php
+++ partner-with-us.php
@@ -4,7 +4,7 @@
   form
 {
   display: block;
-  margin: 100px auto;
+  margin: 100px auto 20px;
   /*width: 950px;*/
   /*width: 100%;*/
   overflow: hidden;
@@ -215,7 +215,7 @@ form > div > label > input:focus, form > div > label > select:focus
               <input placeholder="What State?" tabindex="4" required=""/>
             </label>
           </div>
-          
+
           <div class="col-md-4" style="border-right: 1px solid #ccc; border-bottom: 1px solid #ccc;">
             <label>
               Country
@@ -447,7 +447,7 @@ form > div > label > input:focus, form > div > label > select:focus
               <input placeholder="What is your phone number?" tabindex="1" required=""/>
             </label>
           </div>
-          
+
           <div class="col-md-4" style="border-right: 1px solid #ccc; border-bottom: 1px solid #ccc;">
             <label>
               Date of Birth
@@ -480,13 +480,142 @@ form > div > label > input:focus, form > div > label > select:focus
               </span>
               </label>
             <!-- </a> -->
-            
+
           </div>
 
           <div class="col-xs-12 text-center" style="margin-bottom: 20px;">
           	<input type="submit" value="Register As A Partner" class="btn btn-danger">
           </div>
+
+
         </form>
+
+        <div class="col-xs-12">
+          <h2 class="text-center">Agreement</h2>
+          <div class="agreement">
+            <h4>Privacy Policy</h4>
+
+            <p>At EZYFX we strive to deliver quality products and services to our users. We value their businesses
+and recognize that privacy is an important issue. The information contained in this Privacy Policy
+explains the practices of EZYFX, and its subsidiaries and affiliates (collectively, "EZYFX," "we," or
+"us") regarding the personal or non-personal information that we collect from you or about you
+through this Site. </p>
+
+            <p>“The Site” shall refer collectively to ezyfx.la website and its available language versions and domain names. </p>
+
+            <p>“User” shall refer to any person or entity visiting, using or otherwise accessing the Site.</p>
+
+            <p>Although we aim to ensure the safety and confidentiality of all information received by our clients, please note that this Privacy Policy does not apply to processing of personal information by third parties such as payment service providers. </p>
+
+            <p>If you have any questions about our Privacy Policy, please contact us at <a href="mailto:support@ezyfx.la">support@ezyfx.la</a></p>
+
+            <h5>Registration and Use of User Activities on the Website </h5>
+
+            <h6>Non-Personal Data </h6>
+
+            <p>EZYFX collects non- personal information about Users when they visit The Site unless Users choose to provide that information to us by registering an account. </p>
+
+            <p>EZYFX may record and analyse all User activities on the Site for the purposes of security, system monitoring, management, and marketing in compliance with legal and regulatory obligations incumbent on EZYFX. We undertake to store such information using adequate security measures and for a limited period of time.</p>
+
+            <h6>Log Files</h6>
+
+            <p>Log files allow us to record visitors' use of the Site. We put together log file information from all our visitors and use it to improve our understanding of Users’ patterns of behaviour and needs, and to make improvements to the Site, based on the way users move around it. Log files do not contain any personal information about the User. We automatically collect non-personal information such as: </p>
+
+            <ul class="ppro">
+              <li>&mdash; The type and version of the operating system of User’s computer </li>
+              <li>&mdash; The date and time of User’s access to the Site </li>
+              <li>&mdash; The type and version of the browser used to access the Site </li>
+              <li>&mdash; The IP address from which User accesses the Site</li>
+              <li>&mdash; The pages the user has visited</li>
+              <li>&mdash; If User linked to the Site from another website, the address of that other website; etc. </li>
+            </ul>
+
+            <p>Regularly reviewing this page ensures that Users are always aware of what information we collect and how we use it. </p>
+
+            <h6>Cookies</h6>
+
+            <p>Cookies are small text files sent by a web server and stored on a visitor's browser which are read by the server when Users re-visit a website. Cookies are used to store information such as passwords (encrypted), user-names, interests, shopping preferences, etc., allowing Users to re-visit a website without having to enter the same information again. Cookies are not used to determine the personal identity of visitors to the Site. </p>
+
+            <h6>Personal Data </h6>
+
+            <p>To open a demo or a trading account with EZYFX, Users need to provide certain information (hereinafter referred to as “Personal Data”) about themselves. The Personal Data that we collect may include: </p>
+
+            <ul class="ppro">
+              <li>&mdash; Information provided on applications and other forms on the Site, such as full name, address, date of birth, passport data, or other identification information </li>
+              <li>&mdash; Personal phone number </li>
+              <li>&mdash; Financial information that may include investment experience </li>
+              <li>&mdash; Documentation made available to us to confirm identity, such as ID card, passport, utilities, etc., for individual customers  </li>
+              <li>&mdash; Company incorporation information for business clients </li>
+              <li>&mdash; Payment documents, such as money transfer orders, bank statements, etc.  </li>
+            </ul>
+
+            <h6>Use of Personal Data </h6>
+            <p>EZYFX may use a User's Personal Data for one or more of the purposes below: </p>
+
+            <ul class="ppro">
+              <li>&mdash; To verify User's identity  </li>
+              <li>&mdash; To process trading or non-trading operations </li>
+              <li>&mdash; To perform anti-money laundering procedures </li>
+              <li>&mdash; To keep an accurate and up-to-date record of User’s account data  </li>
+              <li>&mdash; To provide information to the User about specific products and services of EZYFX that we believe will be most suitable for him.  </li>
+            </ul>
+
+            <h6>Confidentiality in Respect of User IDs and Passwords </h6>
+
+            <p>Users are obliged to keep user IDs, passwords and other means of identification secret. Users shall not store their user IDs, passwords or other means of identification in their browsers and shall delete all temporary files stored in their cache memory as well as surfing history. Users are solely responsible should such means of identification be communicated to, discovered or misused by a third party, and for any and all losses and damages that may result therefrom. </p>
+
+            <p>Users have taken note of the fact that they should disconnect from the identification services proposed by EZYFX before leaving their Internet station. </p>
+
+            <h6>Confidentiality and Encryption </h6>
+
+            <p>The data exchanged between Users and EZYFX is transmitted via open, public networks (such as the Internet) that are not subject to any controls or reviews. Even if the data transmitted is encrypted, it may not necessarily stay encrypted for the entire duration of the transmission, nor may it necessarily remain encrypted at User’s end: it is possible that data may be intercepted. </p>
+
+            <p>Furthermore, Users hereby declare that they have taken note that the legislation applicable in their country may prohibit or restrict the importing, exporting or use of encryption logarithms. In no event shall EZYFX be responsible for any violations of measures governing the importing, exporting or use of encryption logarithms. User alone shall be responsible in the event of any such violation.</p>
+
+            <p>Lastly, User attention is drawn to the fact that information transmitted electronically, e.g. by e-mail, SMS, etc., is in principle not encrypted and may be intercepted.</p>
+
+            <p>If you have any questions about this Privacy Policy or how EZYFX processes your personal information, please contact us by email at <a href="mailto:support@ezyfx.la">support@ezyfx.la</a></p>
+
+
+            <h4>RISK DISCLOSURE</h4>
+
+            <p>EZYFX Limited., operating under the trading name EZYFX, having its registered address at S.I.P. Building,
+P.O Box 3010, Rue Pasteur, Port Vila, Vanuatu, registration number 17925, hereinafter collectively referred
+to as “EZYFX”, we”, or “us”, is authorised and recognized by Vanuatu FSC as one of the Registered
+Financial Service Provider (FSP). </p>
+
+            <p>“The Site” shall refer collectively to ezyfx.la website and its available language versions and domain names. </p>
+
+            <p>“Client” shall refer to any person or entity who registered an account at the Site, or who is using, visiting or
+otherwise accessing the Site. </p>
+
+            <p>This document is a disclosure by EZYFX Limited, of the potential risks involved in trading on financial
+            markets. The document cannot and does not disclose all risks and all other considerable aspects inherent in
+            trading on financial markets. The client should first and foremost be aware of the potential losses related to
+            this activity. </p>
+
+            <p>The client hereby acknowledges, understands and agrees that:</p>
+
+            <p><strong>Trading is highly speculative and risky</strong></p>
+
+            <p>Trading CFDs or any other derivate product is highly speculative, involves a high-degree of risk of loss and
+may not be appropriate for all clients, but only for those clients who:</p>
+
+            <ul class="ppro">
+              <li>&mdash; Understand and are willing to assume the economic, legal and other risks and costs involved;  </li>
+              <li>&mdash; Have knowledge and experience in trading in derivatives and in underlying asset types, as well as
+enough time to manage their investment in an active basis; and  </li>
+              <li>&mdash; Are able to assume financial losses significantly in excess of margin or deposits as clients may lose
+the total value of the contract not just the margin or the deposit.  </li>
+            </ul>
+
+            <p>The client is the only responsible person for all the losses suffered in his account. Therefore, the client
+should be prepared that he may lose all of his investments. Clients are advised not to invest money they
+cannot afford to lose. </p>
+
+            <p><strong>Through EZYFX Limited clients can trade CFDs on forex (FX), spot metals, futures and shares. </strong></p>
+          </div>
+        </div>
       </div>
       </div>
     </div>
